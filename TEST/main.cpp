8ca8e5a3bc79106c2b41db173b0893b734270609
mechsoon <ksw5190@gmail.com>
2024-10-15 21:11:28 +0200

    test file updated

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -7,6 +7,7 @@
 #include <QCoreApplication>
 #include <QPropertyAnimation>
 #include <deque>
+#include <QScreen>  // QScreen 추가
 
 #include <DateClock.h>
 #include <receiver.h>
@@ -23,19 +24,7 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
-//    qmlRegisterType<SpeedGauge>("com.myapp.gauge", 1, 0, "SpeedGauge");
-//    qmlRegisterType<RpmGauge>("com.myapp.gauge", 1, 0, "RpmGauge");
-
     QQmlApplicationEngine engine;
-
-//    i2c_fd = open(I2C_BUS, O_RDWR);
-//    if (i2c_fd < 0) {
-//        return -1;
-//    }
-//    if (ioctl(i2c_fd, I2C_SLAVE, INA219_ADDRESS) < 0) {
-//        close(i2c_fd);
-//        return -1;
-//    }
 
     DateClock clock;
     DateClock date;
@@ -44,13 +33,11 @@ int main(int argc, char *argv[])
     receiver.initialize();
     receiver.start();
 
-
     engine.rootContext()->setContextProperty("Clock", &clock);
     engine.rootContext()->setContextProperty("Date", &date);
     engine.rootContext()->setContextProperty("Receiver", &receiver);
 
     Weather weather;
-
     engine.rootContext()->setContextProperty("Weather", &weather);
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
@@ -67,50 +54,23 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-
-
-    /*///////////////////////////////////////////////////////// test Battery gauge with random value */
-//    qreal battery = 0.0;
-//    qreal previousBattery = 0.0;
-//    const qreal b_threshold = 2.0; // 1.0
-
-//    std::deque<qreal> batteryValues;
-//    const int smoothWindowSize = 10; //5
-
-//    std::srand(std::time(nullptr));
-//    QTimer *timer_test_rpm = new QTimer(&app);
-
-//    QPropertyAnimation animation(speedometerObj, "battery");
-//    animation.setDuration(1000);
-//    animation.setEasingCurve(QEasingCurve::OutCubic);
-
-//    QObject::connect(timer_test_rpm, &QTimer::timeout, [&](){
-
-//        battery = readVoltage(i2c_fd);
-//        batteryValues.push_back(battery);
-
-//        if (batteryValues.size() > smoothWindowSize){
-//            batteryValues.pop_front();
-//        }
-
-//        qreal batterySum = std::accumulate(batteryValues.begin(), batteryValues.end(), 0.0) / batteryValues.size();
-//        int batteryPercentage = calculateBatteryPercentage(batterySum); // battery
-
-//        qDebug() << "battery gap : " << batteryPercentage - previousBattery;
-//        if (std::fabs(batteryPercentage - previousBattery) >= b_threshold){
-
-//            animation.setStartValue(speedometerObj->property("battery"));
-//            animation.setEndValue(batteryPercentage); // battery
-//            animation.start();
-//            previousBattery = batteryPercentage;
-
-//        }
-//        engine.rootContext()->setContextProperty("battery_value", batteryPercentage); //static_cast<int>(battery)
-
-//        qDebug() << "Battery : " << battery;
-//        qDebug() << "Battery percentage : " << batteryPercentage;
-//    });
-//    timer_test_rpm->start(1000);
+    // QScreen을 이용한 모니터 선택
+    QList<QScreen*> screens = QGuiApplication::screens();
+    if (screens.size() > 1) {
+        qDebug() << "Second screen detected: " << screens.at(1)->name();
+        QWindow *window = qobject_cast<QWindow*>(object);
+        if (window) {
+            window->setScreen(screens.at(1));  // 두 번째 모니터 설정
+            window->showFullScreen();          // 전체 화면 모드로 실행
+        }
+    } else {
+        qDebug() << "Only one screen detected.";
+        QWindow *window = qobject_cast<QWindow*>(object);
+        if (window) {
+            window->setScreen(screens.at(0));  // 첫 번째 모니터 설정
+            window->showFullScreen();          // 전체 화면 모드로 실행
+        }
+    }
 
     return app.exec();
 }
