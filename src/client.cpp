34f1290d0b86d8e169508205b2ce645d7fd8106b
mechsoon <ksw5190@gmail.com>
2025-05-26 12:31:56 +0200

    tcp to udp

--- src/client.cpp
+++ src/client.cpp
@@ -33,72 +33,115 @@ struct ControlData {
     uint8_t indicator_r;
 };
 
-// Python에서 기어 상태를 받는 함수
-void receive_gear_state() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    // char buffer[1024] = {0};
-    ControlData control_data;
-    // 소켓 생성
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        std::cerr << "Socket creation error" << std::endl;
-        return;
-    }
+void receive_gear_state()
+{
+    int server_fd{}, new_socket{};
+    sockaddr_in address{};
+    int opt      = 1;
+    socklen_t al = sizeof(address);
+    ControlData control{};
 
-    // 포트 번호 재사용 설정
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        std::cerr << "Setsockopt error" << std::endl;
-        return;
-    }
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) { perror("socket"); return; }
 
-    // 주소 설정
-    address.sin_family = AF_INET;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+
+    address.sin_family      = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(65432);
+    address.sin_port        = htons(65432);
 
-    // 소켓에 주소 바인딩
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        std::cerr << "Bind failed" << std::endl;
-        return;
-    }
+    if (bind(server_fd, (sockaddr *)&address, sizeof(address)) < 0) { perror("bind"); return; }
+    if (listen(server_fd, 3) < 0)                                 { perror("listen"); return; }
 
-    // 연결 대기
-    if (listen(server_fd, 3) < 0) {
-        std::cerr << "Listen error" << std::endl;
-        return;
-    }
-
-    std::cout << "Waiting for connection from Python..." << std::endl;
-
-    // 클라이언트 연결 수락
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        std::cerr << "Accept error" << std::endl;
-        return;
-    }
+    std::cout << "Waiting for connection from Python...\n";
+    new_socket = accept(server_fd, (sockaddr *)&address, &al);
+    if (new_socket < 0) { perror("accept"); return; }
 
     while (true) {
-        int valread = read(new_socket, &control_data, sizeof(ControlData));
-        if (valread > 0) {
-            if (control_data.gear_P == 1) gear_state = "P";
-            else if (control_data.gear_D == 1) gear_state = "D";
-            else if (control_data.gear_R == 1) gear_state = "R";
-            else if (control_data.gear_N == 1) gear_state = "N";
+        int n = read(new_socket, &control, sizeof(control));
+        if (n <= 0) break;
 
-            // 속도, 거리 출력
-            std::cout << "Gear: " << gear_state
-                      << " | Throttle: " << control_data.throttle
-                      << " | Steering: " << control_data.steering
-                      << " | Speed: " << control_data.speed << " cm/s"
-                      << " | Distance: " << control_data.distance << " cm"
-                      << std::endl;
-        }
+        if (control.gear_P) gear_state = "P";
+        else if (control.gear_D) gear_state = "D";
+        else if (control.gear_R) gear_state = "R";
+        else if (control.gear_N) gear_state = "N";
+
+        std::cout << "Gear: "      << gear_state
+                  << " | Throttle: " << control.throttle
+                  << " | Steering: " << control.steering
+                  << " | Speed: "    << control.speed     << " cm/s"
+                  << " | Distance: " << control.distance  << " cm\n";
     }
 
     close(new_socket);
     close(server_fd);
 }
+// Python에서 기어 상태를 받는 함수
+// void receive_gear_state() {
+//     int server_fd, new_socket;
+//     struct sockaddr_in address;
+//     int opt = 1;
+//     int addrlen = sizeof(address);
+//     // char buffer[1024] = {0};
+//     ControlData control_data;
+//     // 소켓 생성
+//     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+//         std::cerr << "Socket creation error" << std::endl;
+//         return;
+//     }
+
+//     // 포트 번호 재사용 설정
+//     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+//         std::cerr << "Setsockopt error" << std::endl;
+//         return;
+//     }
+
+//     // 주소 설정
+//     address.sin_family = AF_INET;
+//     address.sin_addr.s_addr = INADDR_ANY;
+//     address.sin_port = htons(65432);
+
+//     // 소켓에 주소 바인딩
+//     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+//         std::cerr << "Bind failed" << std::endl;
+//         return;
+//     }
+
+//     // 연결 대기
+//     if (listen(server_fd, 3) < 0) {
+//         std::cerr << "Listen error" << std::endl;
+//         return;
+//     }
+
+//     std::cout << "Waiting for connection from Python..." << std::endl;
+
+//     // 클라이언트 연결 수락
+//     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+//         std::cerr << "Accept error" << std::endl;
+//         return;
+//     }
+
+//     while (true) {
+//         int valread = read(new_socket, &control_data, sizeof(ControlData));
+//         if (valread > 0) {
+//             if (control_data.gear_P == 1) gear_state = "P";
+//             else if (control_data.gear_D == 1) gear_state = "D";
+//             else if (control_data.gear_R == 1) gear_state = "R";
+//             else if (control_data.gear_N == 1) gear_state = "N";
+
+//             // 속도, 거리 출력
+//             std::cout << "Gear: " << gear_state
+//                       << " | Throttle: " << control_data.throttle
+//                       << " | Steering: " << control_data.steering
+//                       << " | Speed: " << control_data.speed << " cm/s"
+//                       << " | Distance: " << control_data.distance << " cm"
+//                       << std::endl;
+//         }
+//     }
+
+//     close(new_socket);
+//     close(server_fd);
+// }
 
 // vsomeip 요청을 보내는 함수
 void run() {
